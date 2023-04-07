65d44df1275dd09ea4d07f1ccd41ebaeb13f322b
gokulmaxi <gokulakams@gmail.com>
2023-04-07 12:56:34 +0530

    Update sql queries file

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -3,3 +3,24 @@ CREATE TABLE sports (
 	sportsId int PRIMARY KEY IDENTITY,
 	sportsName VARCHAR(50),
 )
+-- create tournament table
+CREATE TABLE tournaments (
+    tournementID int NOT NULL PRIMARY KEY IDENTITY,
+    tournamentName VARCHAR(50) NOT NULL,
+    sportsID int FOREIGN KEY REFERENCES sports(sportsId)
+)
+-- create scoreCard table
+CREATE TABLE scoreCard (
+    scoreCardID int NOT NULL PRIMARY KEY IDENTITY,
+    MatchName VARCHAR(50) NOT NULL,
+	teamAScore int NOT NULL,
+	teamBScore int not NULL,
+	winner VARCHAR(50),
+    tournamentID int FOREIGN KEY REFERENCES tournaments(tournementID)
+)
+SELECT scoreCardID,MatchName,teamAScore,teamBScore,winner FROM scoreCard
+--Trigger to update the winner
+-- test query for viewing tournaments
+SELECT *
+FROM tournaments
+INNER JOIN sports ON tournaments.sportsID=sports.sportsId
